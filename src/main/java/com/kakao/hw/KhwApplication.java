57e3c2732d3e3de46e0bcb26daf634a2a07d2c7f
YOON <yoonz44@naver.com>
2019-04-02 21:33:13 +0900

    first KHW project set

---
+++ src/main/java/com/kakao/hw/KhwApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhwApplication.class, args);
+	}
+
+}
